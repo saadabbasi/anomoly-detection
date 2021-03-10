87679d9bd2d471f096dd6815fd09774713564955
Saad Abbasi <saad.rabbasi@gmail.com>
2021-03-10 03:55:11 -0500

    reads cropped mel-spectograms

--- mimii_dataset.py
+++ mimii_dataset.py
@@ -3,59 +3,44 @@ import numpy as np
 import os
 import sklearn.preprocessing
 
-_DATA_DIR = "features"
+_NORMAL_DATA_DIR = "spectograms/normal"
+_ABNORMAL_DATA_DIR = "spectograms/abnormal"
 _TRAIN_BATCH_SIZE = 32
-_STEP = 32
 
 class MIMIIDataset:
     def __init__(self):
-        self.data_dir = _DATA_DIR
+        self.normal_data_dir = _NORMAL_DATA_DIR
+        self.abnormal_data_dir = _ABNORMAL_DATA_DIR
         self.train_batch_size = 1
+        self.test_batch_size = 512
         self.normalizer = sklearn.preprocessing.StandardScaler()
-        self.step = _STEP
+        self.dim = (32,128)
+
+        self._read_dataset()
 
     def get_train_dataset(self, num_shards=1, shard_index=0):
-        self.dim = (32,128)
-        x_train = np.load(os.path.join(self.data_dir,"train_6dB_fan_id_06.npy"))
-        # nsamples, nx, ny = x_train.shape
-        # x_train_flat = x_train.reshape((nsamples,nx*ny))
-        # x_train_flat = self.normalizer.fit_transform(x_train_flat)
-        # x_train = x_train_flat.reshape((nsamples, nx, ny))
-        x_train = x_train / np.max(x_train)
+        return self.train_x/self.train_x.max(), self.train_y
 
-        self.indexes_start = np.arange(x_train.shape[1]-self.dim[0]+self.step, step=self.step)
-        self.max = len(self.indexes_start)
-        self.indexes = np.arange(x_train.shape[0])
-        self.indexes = np.repeat(self.indexes, self.max)
-        self.indexes_start = np.repeat(self.indexes_start, x_train.shape[0])
-        np.random.shuffle(self.indexes)
-        np.random.shuffle(self.indexes_start)
-
-        num_batches = int(np.floor(x_train.shape[0] * self.max  / self.train_batch_size))
-
-        x_train_cropped = np.empty((num_batches,*self.dim))
-        for index in range(num_batches):
-            indexes = self.indexes[index*self.train_batch_size:(index+1)*self.train_batch_size]
-            indexes_start = self.indexes_start[index*self.train_batch_size:(index+1)*self.train_batch_size]
-            x = self.__data_generation(x_train, indexes, indexes_start)
-            x_train_cropped[index*self.train_batch_size:(index+1)*self.train_batch_size,:] = self.__data_generation(x_train, indexes, indexes_start)
-        
-        self.train_batch_size = 32
-        x_train_cropped  = np.expand_dims(x_train_cropped,-1)
-        print(x_train_cropped.shape)
-        trainset = tf.data.Dataset.from_tensor_slices((x_train_cropped, x_train_cropped)).batch(self.train_batch_size)
-        trainset = trainset.shuffle(self.train_batch_size)
-        return trainset
-    
     def get_test_dataset(self, num_shards=1, shard_index=0):
-        x_test = np.load(os.path.join(self.data_dir,"test_6dB_fan_id_00.npy"))
-        nsamples, nx, ny = x_test.shape
-        x_test_flat = x_test.reshape((nsamples,nx*ny))
-        x_test_flat = self.normalizer.transform(x_test_flat)
-        x_test = x_test_flat.reshape((nsamples, nx, ny))
+        return self.test_x/self.test_x.max(), self.test_y
 
-        testset = tf.data.Dataset.from_tensor_slices((x_test,x_test)).batch(self.train_batch_size)
-        return testset
+    def _read_dataset(self):
+        normal = np.load(os.path.join(self.normal_data_dir,"normal_6dB_fan_id_06.npy"))
+        abnormal = np.load(os.path.join(self.abnormal_data_dir,"abnormal_6dB_fan_id_06.npy"))
+
+        normal_labels = np.zeros((len(normal)))
+        abnormal_labels = np.ones((len(abnormal)))
+        train_x = normal[abnormal.shape[0]:]
+        train_y = normal_labels[abnormal.shape[0]:]
+        test_x = normal[:abnormal.shape[0]]
+        test_y = normal_labels[:abnormal.shape[0]]
+        test_x = np.concatenate((test_x, abnormal))
+        test_y = np.concatenate((test_y, abnormal_labels))
+        
+        self.train_x = np.expand_dims(train_x,axis=-1)
+        self.test_x = np.expand_dims(test_x,axis=-1)
+        self.train_y = train_y
+        self.test_y = test_y
 
     def __data_generation(self, data, indexes, index_start):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
