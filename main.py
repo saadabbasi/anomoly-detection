29c1a4c44474e1b82ff949a54916bcd8e2f01b98
Saad Abbasi <saad.rabbasi@gmail.com>
2021-03-22 19:53:45 -0400

    argparse

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sklearn import metrics
 import numpy as np
 import mimii_dataset
 import tf_models
+import argparse
 
 def compute_probs(output_imgs, target_imgs):
     # should I be calling these imgs?
@@ -36,57 +37,62 @@ def compute_AUC(output_imgs, target_imgs, y_true, plot_hist=False):
     auc = metrics.roc_auc_score(y_true, y_pred)
     return auc
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', type = str, action = 'store', dest = 'output_name')
+    parser.add_argument('-m', type = int, action = 'store', dest = 'enlarge_by')
+    parser.add_argument('-gpu', type = str, action = 'store', dest = 'gpu')
+    args = parser.parse_args()
 
-tf.set_random_seed = 42
-np.random.seed(42)
-os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+    tf.set_random_seed = 42
+    np.random.seed(42)
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
 
-epochs = 100
-n_mels=128
-frames=5
-n_fft=1024
-hop_length=512
-power=1.0
-batch_size = 16
-step = 3
-scaling = False
+    epochs = 100
+    n_mels=128
+    frames=5
+    n_fft=1024
+    hop_length=512
+    power=1.0
+    batch_size = 16
+    step = 3
+    scaling = False
 
-spectrograms_fnames = ['0dB_fan_id_00','0dB_fan_id_02','0dB_fan_id_04','0dB_fan_id_06',
-                        'min6dB_fan_id_00', 'min6dB_fan_id_02', 'min6dB_fan_id_04', 'min6dB_fan_id_06',
-                        '6dB_fan_id_00', '6dB_fan_id_02', '6dB_fan_id_04','6dB_fan_id_06',
-                        '0dB_slider_id_00','0dB_slider_id_02','0dB_slider_id_04','0dB_slider_id_06',
-                        'min6dB_slider_id_00','min6dB_slider_id_02','min6dB_slider_id_04','min6dB_slider_id_06',
-                        '6dB_slider_id_00','6dB_slider_id_02','6dB_slider_id_04','6dB_slider_id_06']
+    spectrograms_fnames = ['0dB_fan_id_00','0dB_fan_id_02','0dB_fan_id_04','0dB_fan_id_06',
+                            'min6dB_fan_id_00', 'min6dB_fan_id_02', 'min6dB_fan_id_04', 'min6dB_fan_id_06',
+                            '6dB_fan_id_00', '6dB_fan_id_02', '6dB_fan_id_04','6dB_fan_id_06',
+                            '0dB_slider_id_00','0dB_slider_id_02','0dB_slider_id_04','0dB_slider_id_06',
+                            'min6dB_slider_id_00','min6dB_slider_id_02','min6dB_slider_id_04','min6dB_slider_id_06',
+                            '6dB_slider_id_00','6dB_slider_id_02','6dB_slider_id_04','6dB_slider_id_06']
 
-spectrograms_fnames = ['0dB_slider_id_00','0dB_slider_id_02','0dB_slider_id_04','0dB_slider_id_06',
-                        'min6dB_slider_id_00','min6dB_slider_id_02','min6dB_slider_id_04','min6dB_slider_id_06',
-                        '6dB_slider_id_00','6dB_slider_id_02','6dB_slider_id_04','6dB_slider_id_06']
+    spectrograms_fnames = ['0dB_slider_id_00','0dB_slider_id_02','0dB_slider_id_04','0dB_slider_id_06',
+                            'min6dB_slider_id_00','min6dB_slider_id_02','min6dB_slider_id_04','min6dB_slider_id_06',
+                            '6dB_slider_id_00','6dB_slider_id_02','6dB_slider_id_04','6dB_slider_id_06']
 
-fbaseline = open("baseline__.txt","w")
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    fbaseline = open(f"{args.output_name}","w")
 
-for machine in spectrograms_fnames:
-    ds = mimii_dataset.MIMIIDataset(machine_type_id = machine)
-    train_x, _ = ds.train_dataset()
-    test_x, eval_labels = ds.test_dataset()
-    raw = np.mean(test_x,axis=-1).mean(axis=-1).mean(axis=-1)
+    for machine in spectrograms_fnames:
+        ds = mimii_dataset.MIMIIDataset(machine_type_id = machine)
+        train_x, _ = ds.train_dataset()
+        test_x, eval_labels = ds.test_dataset()
+        raw = np.mean(test_x,axis=-1).mean(axis=-1).mean(axis=-1)
 
-    model = tf_models.get_autoencoder_model_s(tf_models.standard_conv(), use_batchnorm=True, enlarge_by=6)
+        model = tf_models.get_autoencoder_model_s(tf_models.standard_conv(), use_batchnorm=True, enlarge_by=args.enlarge_by)
 
-    preds = model.predict(test_x)
-    auc_prior = compute_AUC(preds, test_x, eval_labels, plot_hist=True)
-    y_pred = compute_probs(preds, test_x)
+        preds = model.predict(test_x)
+        auc_prior = compute_AUC(preds, test_x, eval_labels, plot_hist=True)
+        y_pred = compute_probs(preds, test_x)
 
 
-    history = model.fit(x=train_x,y=train_x, epochs=epochs,verbose=1)
-    preds = model.predict(test_x)
-    y_pred = compute_probs(preds, test_x)
-    auc_after = compute_AUC(preds, test_x, eval_labels, plot_hist=True)
+        history = model.fit(x=train_x,y=train_x, epochs=epochs,verbose=1)
+        preds = model.predict(test_x)
+        y_pred = compute_probs(preds, test_x)
+        auc_after = compute_AUC(preds, test_x, eval_labels, plot_hist=True)
 
-    sound_lvl, machine_typ, _, machine_id = machine.split('_')
-    result_str = f"{sound_lvl},{machine_typ},{machine_id},{auc_after},{auc_prior}\n"
-    print(result_str)
-    fbaseline.write(result_str)
-    fbaseline.flush()
+        sound_lvl, machine_typ, _, machine_id = machine.split('_')
+        result_str = f"{sound_lvl},{machine_typ},{machine_id},{auc_after},{auc_prior}\n"
+        print(result_str)
+        fbaseline.write(result_str)
+        fbaseline.flush()
 
-fbaseline.close()
+    fbaseline.close()
