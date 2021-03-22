699d2761f5fb2a0992a81da68461b4b6b7b0f9b8
Saad Abbasi <saad.rabbasi@gmail.com>
2021-03-22 02:51:28 -0400

    methods to measure openvino and tf network latency

--- measure_model_metrics.py
+++ measure_model_metrics.py
@@ -3,6 +3,38 @@ import numpy as np
 import os
 import mimii_dataset
 from time import perf_counter_ns
+import openvino
+from openvino.inference_engine import IENetwork, IECore
+import os
+
+def benchmark_tf_model_ns(sess, OUTPUT_TENSOR, feed_dict, N = 100):
+    time_t = np.zeros((N,))
+    for n in range(N):
+        start_t = perf_counter_ns()
+        out = sess.run([OUTPUT_TENSOR], feed_dict = feed_dict)
+        end_t = perf_counter_ns()
+        time_t[n] = end_t - start_t
+
+    return time_t.min()
+
+def benchmark_openvino_model_ns(model, shape, N = 100):
+    etime = np.zeros((N,))
+    if type(model) == openvino.inference_engine.ie_api.ExecutableNetwork:
+        x = np.random.randn(shape[0],shape[1],shape[2],shape[3])
+        for i in range(100):
+            start_t = perf_counter_ns()
+            model.infer({'input_1':x})
+            end_t = perf_counter_ns()
+            etime[i] = end_t - start_t
+    
+    return etime.min()
+
+def load_openvino_model(xml_path):
+    bin_path = os.path.splitext(xml_path)[0] + ".bin"
+    ie = IECore()
+    net = ie.read_network(model=xml_path, weights = bin_path)
+    exec_net = ie.load_network(network = net, device_name='CPU', num_requests=0)
+    return exec_net
 
 def load_frozen_graph(frozen_graph_filename):
     import tensorflow as tf
@@ -33,16 +65,6 @@ def get_flops(sess, run_meta):
 
     print("{:,} --- {:,}".format(flops.total_float_ops, params.total_parameters))
 
-def tf_network_latency(sess, OUTPUT_TENSOR, feed_dict, N = 100):
-    time_t = np.zeros((N,))
-    for n in range(N):
-        start_t = perf_counter_ns()
-        out = sess.run([OUTPUT_TENSOR], feed_dict = feed_dict)
-        end_t = perf_counter_ns()
-        time_t[n] = end_t - start_t
-
-    return time_t.min()
-
 # IMAGE_INPUT_TENSOR = "input_1:0"
 # TARGET_INPUT_TENSOR = "separable_conv2d_6_target:0"
 # OUTPUT_TENSOR = "separable_conv2d_6/BiasAdd:0"
@@ -61,6 +83,11 @@ ckpt_path = model_dir
 device_to_run = ""
 os.environ["CUDA_VISIBLE_DEVICES"] = device_to_run
 
+xml_path = os.path.join(model_dir, "frozen.xml")
+model = load_openvino_model(xml_path)
+openvino_min = benchmark_openvino_model_ns(model, (1,1,32,128)) / 1e6
+print(f"openvino: {openvino_min/1e6}")
+
 ds = mimii_dataset.MIMIIDataset('min6dB_slider_id_00', format = '3D', train_batch_size=1, test_batch_size=1)
 
 test_x, test_y = ds.test_dataset()
@@ -73,5 +100,5 @@ graph,sess,saver = load_graph(meta_path)
 
 with sess.as_default():
     load_ckpt(sess, saver, ckpt_path)
-    min = tf_network_latency(sess, OUTPUT_TENSOR, feed_dict, N = 10) / 1e6
-    print(f"Min: {min:.3f} µs")
+    tf_min = benchmark_tf_model_ns(sess, OUTPUT_TENSOR, feed_dict, N = 10) / 1e6
+    print(f"OpenvinO: {openvino_min:.3f} µs, Tensorflow: {tf_min:.3f} µs")
